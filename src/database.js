5e464c7736d6128a3d9011c4ef31e20f7422f5a5
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-05-21 16:49:36 -0500

    creacion de tablas de la base de datos de manera local

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const {Pool} = require('pg')
 const pool = new Pool({
   user: 'agromerque',
   password: 'agromerque',
-  host: 'localhost',
+  host: '0.0.0.0',
   port: 5432,
   database: 'agromerque',
 });
