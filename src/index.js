823093b5b9960e81e1c953ed0f74a192fd304702
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-05-25 17:09:12 -0500

    crear user afecta tablas customer o admin y encriptacion de la contrasena

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ const app = express();
 const productRoutes = require ('./routes/product.routes')
 const userRoutes = require ('./routes/user.routes')
 const customerRoutes = require ('./routes/customer.routes')
+const categoryRoutes = require ('./routes/category.routes')
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(productRoutes)
 app.use(userRoutes)
 app.use(customerRoutes)
+app.use(categoryRoutes)
 app.listen(4000);
 
 console.log('Server is running on port 4000');
